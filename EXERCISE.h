008e806cc7246ba7db6425e3ca6cd8a14e76d342
Sofian Brabez <sbz@6dev.net>
2019-02-19 21:03:13 +0000

    Part 1

--- EXERCISE.h
+++ EXERCISE.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <string>
+
 //This is for part 1
 class ILogger {
-
+    public:
+        virtual ILogger& LogInfo(const std::string& msg) = 0;
+        virtual ILogger& LogError(const std::string& msg) = 0;
+        virtual const std::string& GetLastError() const = 0;
+        virtual ~ILogger() = default;
 };
 
 // This is for Part 2/3
